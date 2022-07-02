743552f59701c7b5237e2689edc3cbb101c0b591
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-07-02 13:01:59 +0300

    ft: add customer and caterer views separately

--- meal/mealapp/urls.py
+++ meal/mealapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/customers/', views.CustomerView.as_view()),
+    path('api/v1/users/', views.CustomerView.as_view()),
 ]
--- meal/mealapp/views.py
+++ meal/mealapp/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 
 # Create your views here.
 
-class CustomerView(APIView):
+class UsersView(APIView):
     
     def get(self, request, format=None):
 
@@ -16,6 +16,32 @@ class CustomerView(APIView):
 
         return Response(serializerdata.data)
     
+    def post(self, request, format=None):
+        serializerdata = CustomUserSerializer(data=request.data)
+        if serializerdata.is_valid():
+            serializerdata.save()
+            return Response(serializerdata.data)
+        return Response(serializerdata.errors)
+
+class CustomerView(APIView):
+    def get(self, request, format=None):
+        customers = CustomUser.objects.filter(is_customer=True)
+        serializerdata = CustomUserSerializer(customers, many=True)
+        return Response(serializerdata.data)
+
+    def post(self, request, format=None):
+        serializerdata = CustomUserSerializer(data=request.data)
+        if serializerdata.is_valid():
+            serializerdata.save()
+            return Response(serializerdata.data)
+        return Response(serializerdata.errors)
+
+class CatererView(APIView):
+    def get(self, request, format=None):
+        caterers = CustomUser.objects.filter(is_caterer=True)
+        serializerdata = CustomUserSerializer(caterers, many=True)
+        return Response(serializerdata.data)
+
     def post(self, request, format=None):
         serializerdata = CustomUserSerializer(data=request.data)
         if serializerdata.is_valid():
