b3a539754d8d67dd2781ccd794efca56cf9f0802
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-07-03 11:46:47 +0300

    fx: merge errors

--- meal/mealapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/v1/users/', views.UsersView.as_view()),
-    path('api/v1/caterers/', views.CatererView.as_view()),
-    path('api/v1/users/', views.CustomerView.as_view()),
-]
--- mealangular/src/app/services/auth-user.service.ts
+++ mealangular/src/app/services/auth-user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const baseUrl = 'http://127.0.0.1:8000/api/v1/customers/'
+const baseUrl = 'http://127.0.0.1:8000/api/v1/users/'
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
